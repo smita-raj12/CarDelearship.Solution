d9aa590214cea90d901c82afae3baf64193a48a2
smita Raj <smita.raj12@gmail.com>
2021-09-14 08:49:30 -0700

    application for car delearship

---
+++ carDelear/Models/Car.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace Dealership.Models {
+
+  public class Car
+  {
+    private string _makeModel;
+    private int _price;
+    private int _miles;
+
+    public Car(string makeModel, int price, int miles)
+    {
+      _makeModel = makeModel;
+      _price = price;
+      _miles = miles;
+    }
+
+    public string GetMakeModel()
+    {
+      return _makeModel;
+    }
+
+    public int GetPrice()
+    {
+      return _price;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
+    }
+    public void SetPrice(int newPrice)
+    {
+      _price = newPrice;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (_price <= maxPrice);
+    }
+  }
+
+}
